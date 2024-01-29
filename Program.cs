b25e88e96f8896bed144d49c67111f45376bc03a
tolyan <arg-r@mail.ru>
2024-01-29 22:00:46 +0300

    НАчали выполнение

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Sem_07 {
+	public class Task_01{ 
+		public static void Main (string [] args){
+			Console.Write ("введите размер массива: ");
+			int size = Convert.ToInt32(Console.ReadLine ());
+			string [] array = new string [size];
+			Console.WriteLine ("Заполним массив");
+			for (int i = 0; i < size; i++){
+				Console.Write ($"введите {i}-й Элемент: ");
+				array [i] = Console.ReadLine();
+			}
+			Console.WriteLine ("Массив заполнен");
+			string [] result = LessOfThreeChar (array);
+			foreach (string item in result) {
+                Console.Write (item + " ");
+            }
+		}
+		public static string [] LessOfThreeChar (string[] array){
+			int n = 0;
+			for (int i = 0; i<array.Length; i++) {
+				if (array[i].Length <= 3) n++;
+			}
+			string [] result = new string [n];
+			int j = 0;	
+			for (int i = 0; i<array.Length; i++){
+				if (array[i].Length <= 3){ 
+					result [j] = array[i];
+					j++;	
+				}
+            }    
+			return result;
+		}
+	}
+}
+
